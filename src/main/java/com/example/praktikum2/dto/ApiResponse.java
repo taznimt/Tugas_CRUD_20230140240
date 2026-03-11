74455327584051e1ac1198e6ebeddddcc5230e0e
taznimt <taznimanwar06@gmail.com>
2026-03-11 16:36:21 +0800

    add generic ApiResponse wrapper

---
+++ src/main/java/com/example/praktikum2/dto/ApiResponse.java
@@ -0,0 +1,15 @@
+package com.example.praktikum2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private String message;
+    private T data;
+}
